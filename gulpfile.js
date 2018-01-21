86d7ba51737c6eda6a3076d79ded1e4387233f0f
pure-js <evolution.is.already.underway@gmail.com>
2018-01-21 23:53:13 +0300

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,42 @@
-var gulp = require('gulp'),
+const gulp = require('gulp'),
   pug = require('gulp-pug'),
   plumber = require('gulp-plumber'),
   stylus = require('gulp-stylus');
-var sourcemaps = require('gulp-sourcemaps');
-var ghPages = require('gulp-gh-pages');
+const sourcemaps = require('gulp-sourcemaps');
+const ghPages = require('gulp-gh-pages');
 
 
-gulp.task('html', function() {
+function html () {
   return gulp.src('./*.jade')
     .pipe(plumber())
     .pipe(pug({
       pretty: true
     }))
     .pipe(gulp.dest('./build/'))
-});
+};
 
-gulp.task('css', function () {
+function css () {
   return gulp.src('./stylesheets/main.styl')
     .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(stylus())
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('./build/css/'));
-});
+};
 
-gulp.task('copy', function() {
-   return gulp.src('./img/*.{png,jpg}')
-   .pipe(gulp.dest('./build/img'));
-});
+function copy () {
+  return gulp.src('./img/*.{png,jpg}')
+    .pipe(gulp.dest('./build/img'));
+};
 
-gulp.task('watch', function() {
-  gulp.watch('./*.jade', ['html']);
-  gulp.watch(['./stylesheets/main.styl', './blocks/**/*.styl'], ['css']);
-});
+function watch() {
+  gulp.watch('./*.jade', html);
+  gulp.watch(['./stylesheets/main.styl', './blocks/**/*.styl'], css);
+};
 
 gulp.task('deploy', function() {
   return gulp.src('./build/**/*')
     .pipe(ghPages());
 });
 
-gulp.task('default', ['html', 'copy', 'css', 'watch'], function() {
-  // place code for your default task here
-});
+gulp.task('default', gulp.series(html, copy, css, watch));
